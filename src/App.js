8955243850e4304d3300f6a0dbe3f1ff08a0d937
James Watling <james.watling@gmail.com>
2020-06-27 17:57:19 +0100

    basic working prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Unsplash, { toJson } from "unsplash-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const unsplash = new Unsplash({
+  accessKey: "W1KhacP9iCqREq87b8S213BNKVqRCIokJFAck0ZgZ-0",
+  secret: "4Y_XejbSlq31ZHyYxFpCEN77D7tezLS486k6QwbeTbk",
+});
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: "",
+      results: [],
+      images: [],
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  formSubmitted(event) {
+    event.preventDefault();
+
+    this.setState({ images: [] });
+
+    unsplash.search
+      .photos(this.state.searchTerm, 1, 20)
+      .then(toJson)
+      .then((result) => {
+        console.log(result);
+        console.log(result.results[0].urls.regular);
+        this.setState({ results: result.results });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>React Image Picker</h1>
+        <form onSubmit={(event) => this.formSubmitted(event)}>
+          <label htmlFor="searchTerm">Search Term</label>
+          <input
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.searchTerm}
+            type="text"
+            className="u-full-width"
+            id="searchTerm"
+            name="searchTerm"
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        <section className="images">
+          {this.state.results.map((image) => {
+            return <img src={image.urls.regular} alt={image.alt_description} />;
+          })}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
